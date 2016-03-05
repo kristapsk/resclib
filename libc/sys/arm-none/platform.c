94fbd6bc7caad9cdca7b41242140ced481a08f70
Kristaps Kaupe <kristaps@blogiem.lv>
2016-03-06 01:26:35 +0200

    Cleanup ARM none platform code
    Testing code (ARM Integrator CP board) was left.

--- libc/sys/arm-none/platform.c
+++ libc/sys/arm-none/platform.c
@@ -2,10 +2,6 @@
 
 #include <unistd.h>
 
-#define SERIAL_BASE 0x16000000
-#define SERIAL_FLAG_REGISTER 0x18
-#define SERIAL_BUFFER_FULL (1 << 5)
-
 ssize_t __read (int fd, void* buf, size_t nbytes)
 {
     // Must be implemented
@@ -14,13 +10,5 @@ ssize_t __read (int fd, void* buf, size_t nbytes)
 ssize_t __write (int fd, const void* buf, size_t nbytes)
 {
     // Must be implemented
-    const char* cbuf = (const char*)buf;
-    size_t i;
-    for (i = 0; i < nbytes; i++) {
-        while (*(volatile unsigned long*)(SERIAL_BASE + SERIAL_FLAG_REGISTER) 
-                                               & (SERIAL_BUFFER_FULL));
-        *(volatile unsigned long*)SERIAL_BASE = *cbuf++;
-    }
-    return i;
 }
 
