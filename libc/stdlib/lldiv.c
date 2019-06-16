cd60e0e13ec7f0d4d4cc1f3370ad1f5d1f9db09b
Kristaps Kaupe <kristaps@blogiem.lv>
2019-06-16 14:03:45 +0300

    Silent false positive GCC8 warning

--- libc/stdlib/lldiv.c
+++ libc/stdlib/lldiv.c
@@ -11,8 +11,12 @@ lldiv_t __lldiv (long long numer, long long denom)
 lldiv_t lldiv (long long number, long long denom)
     _WEAK_ALIAS_OF("__lldiv");
 
+// intmax_t is the same as long long, silent GCC8 incompatible type warning
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wattribute-alias"
 imaxdiv_t imaxdiv (intmax_t number, intmax_t denom)
     _WEAK_ALIAS_OF("__lldiv");
+#pragma GCC diagnostic pop
 
 #ifdef __cplusplus
 }
